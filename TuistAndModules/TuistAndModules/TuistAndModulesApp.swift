1584a2a6ad3954428007a0eaaf8228423d709fd5
Allen <allensoberano@gmail.com>
2025-08-01 21:10:54 -0700

    init project

---
+++ TuistAndModules/TuistAndModules/TuistAndModulesApp.swift
@@ -0,0 +1,17 @@
+//
+//  TuistAndModulesApp.swift
+//  TuistAndModules
+//
+//  Created by Allen Soberano on 8/1/25.
+//
+
+import SwiftUI
+
+@main
+struct TuistAndModulesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
